d66757e687c0f55b51667d0a68330d4ffcd5feac
IGP <pyltsov@gmail.com>
2021-06-27 14:00:38 +0300

    +

---
+++ Hackathon3/ImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCollectionViewCell.swift
+//  Hackathon3
+//
+//  Created by ИГОРЬ on 26/06/2021.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Hackathon3/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Hackathon3
+//
+//  Created by ИГОРЬ on 26/06/2021.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class Model {
+    
+}
+
